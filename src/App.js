badd0ef979af7c9a71b5a603e5ea7063f4ae1e00
Alexane J <alexanetouti@hotmail.Fr>
2021-05-23 15:02:38 +0200

    search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.jpg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"></img>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="myFooter">Coded by Alexane Jouglar | Open source code available on GitHub</footer>
+      </div>
     </div>
   );
 }
